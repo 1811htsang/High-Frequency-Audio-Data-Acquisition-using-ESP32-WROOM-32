1cda7db6894c42d920827e520825d07ca4ed7389
Huỳnh Thanh Sang <macsin233@gmail.com>
2026-01-12 21:58:46 +0700

    feat(lib): add header file for clock definitions and register structures

---
+++ project-artifactspace/lib/lib_clock_definition.h
@@ -0,0 +1,14 @@
+#ifndef LIB_CLOCK_DEFINITION_H_
+  #define LIB_CLOCK_DEFINITION_H_
+
+  // Khai báo địa chỉ ngoại vi
+
+  // Khai báo địa chỉ thanh ghi cụ thể
+
+  // Khai báo cấu trúc thanh ghi
+
+  // Khai báo con trỏ tới thanh ghi
+
+  // Khai báo các định nghĩa bit cần sử dụng 
+
+#endif /* LIB_CLOCK_DEFINITION_H_ */
