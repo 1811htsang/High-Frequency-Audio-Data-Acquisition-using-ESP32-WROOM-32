d48dc50b3f9f145ba26bc28f9d46ece2544cb694
Huỳnh Thanh Sang <macsin233@gmail.com>
2026-02-23 16:35:10 +0700

    feat(main): enhance I2S configuration with standard mode setup

--- project-artifactspace/main/main.c
+++ project-artifactspace/main/main.c
@@ -99,8 +99,28 @@
 
     // Cấu hình I2S
 
+      // Khởi tạo I2S channel
       i2s_chan_handle_t rx_handle;
-
       i2s_chan_config_t chan_cfg = I2S_CHANNEL_DEFAULT_CONFIG(I2S_NUM_0, I2S_ROLE_MASTER);
+      i2s_new_channel(&chan_cfg, NULL, &rx_handle);
+
+      // Cấu hình I2S chuẩn
+      i2s_std_config_t std_cfg = {
+        .clk_cfg = I2S_STD_CLK_DEFAULT_CONFIG(48000),
+        .slot_cfg = I2S_STD_MSB_SLOT_DEFAULT_CONFIG(I2S_DATA_BIT_WIDTH_32BIT, I2S_SLOT_MODE_STEREO),
+        .gpio_cfg = {
+          .mclk = I2S_GPIO_UNUSED,
+          .bclk = GPIO_NUM_4,
+          .ws = GPIO_NUM_5,
+          .dout = I2S_GPIO_UNUSED,
+          .din = GPIO_NUM_19,
+          .invert_flags = {
+              .mclk_inv = false,
+              .bclk_inv = false,
+              .ws_inv = false,
+          },
+        },
+      };
+      i2s_channel_init_std_mode(rx_handle, &std_cfg);
 
   }
