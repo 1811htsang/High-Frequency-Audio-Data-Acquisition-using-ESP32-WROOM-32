990f8503521dda74c6707753e43cb7a4fd635d2a
Huỳnh Thanh Sang <macsin233@gmail.com>
2026-02-27 22:17:45 +0700

    feat(i2s): update I2S standard configuration for 96kHz sample rate and external clock source

--- project-artifactspace/main/main.c
+++ project-artifactspace/main/main.c
@@ -104,12 +104,17 @@
       i2s_chan_config_t chan_cfg = I2S_CHANNEL_DEFAULT_CONFIG(I2S_NUM_0, I2S_ROLE_MASTER);
       i2s_new_channel(&chan_cfg, NULL, &rx_handle);
 
-      // Cấu hình I2S chuẩn
+      // Cấu hình I2S định dạng chuẩn
       i2s_std_config_t std_cfg = {
-        .clk_cfg = I2S_STD_CLK_DEFAULT_CONFIG(48000),
+        .clk_cfg = {
+          .sample_rate_hz = 96000,
+          .clk_src = I2S_CLK_SRC_EXTERNAL,
+          .mclk_multiple = I2S_MCLK_MULTIPLE_256,
+          .ext_clk_freq_hz = 24576000
+        },
         .slot_cfg = I2S_STD_MSB_SLOT_DEFAULT_CONFIG(I2S_DATA_BIT_WIDTH_32BIT, I2S_SLOT_MODE_STEREO),
         .gpio_cfg = {
-          .mclk = I2S_GPIO_UNUSED,
+          .mclk = GPIO_NUM_0,
           .bclk = GPIO_NUM_4,
           .ws = GPIO_NUM_5,
           .dout = I2S_GPIO_UNUSED,
@@ -123,4 +128,24 @@
       };
       i2s_channel_init_std_mode(rx_handle, &std_cfg);
 
+      /**
+       * Ghi chú:
+       * Thiết kế đảm bảo theo tài liệu của PCM1808, ta sẽ đạt được
+       * - Tốc độ lấy mẫu 96kHz (dùng trong đầu ra của pin WS/LRCK)
+       * - Độ phân giải 24-bit (sử dụng 32-bit slot để chứa dữ liệu 24-bit theo định dạng MSB-first & Left-justified)
+       * - Sử dụng nguồn clock ngoài 24.576MHz (được cung cấp qua pin MCLK) để đảm bảo độ chính xác của tốc độ lấy mẫu, 
+       * tránh sai lệch do clock nội bộ có thể không ổn định hoặc bị ảnh hưởng bởi các yếu tố khác.
+       * - BCK đạt tần số 6.144MHz (tính toán từ MCLK 24.576MHz chia cho 4, phù hợp với tốc độ lấy mẫu 96kHz và độ phân giải 24-bit)
+       * - Cấu hình GPIO phù hợp với sơ đồ kết nối phần cứng đã thiết kế, đảm bảo tín hiệu được truyền đúng cách giữa ESP32 và PCM1808.
+       */
+
+
+      /* Before reading data, start the RX channel first */
+        // i2s_channel_enable(rx_handle);
+        // i2s_channel_read(rx_handle, desc_buf, bytes_to_read, bytes_read, ticks_to_wait);
+
+      /* Have to stop the channel before deleting it */
+        // i2s_channel_disable(rx_handle);
+      /* If the handle is not needed any more, delete it to release the channel resources */
+        // i2s_del_channel(rx_handle);
   }
